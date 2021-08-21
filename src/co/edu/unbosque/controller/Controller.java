ed6ed184f2e1bbb0f703845d83703c5e774e7f90
estebanCastr0 <83050986+estebanCastr0@users.noreply.github.com>
2021-08-20 19:19:40 -0500

    Se agregan ejecuciones en el controlador

--- src/co/edu/unbosque/controller/Controller.java
+++ src/co/edu/unbosque/controller/Controller.java
@@ -1,9 +1,19 @@
 package co.edu.unbosque.controller;
 
+import co.edu.unbosque.model.Ordenamiento;
+import co.edu.unbosque.view.VistaConsola;
+
 public class Controller {
 
+    private VistaConsola v;
+    private Ordenamiento o;
+    int numerof;
+
     public Controller (){
-        System.out.println("hola paco");
+        v = new VistaConsola();
+        System.out.print(Integer.parseInt(v.getTamano()));
+        o = new Ordenamiento( Integer.parseInt(v.getTamano()));
+        //v.leerDato(o.buscarNumero());
     }
 
 }
